13f50cd75b3047b7272e00f028d951e06f781723
GearBoxFox <98189655+GearBoxFox@users.noreply.github.com>
2024-08-13 11:51:47 -0400

    create actual stuff in pivot/turret

--- src/main/java/frc/robot/subsystems/Pivot.java
+++ src/main/java/frc/robot/subsystems/Pivot.java
@@ -1,4 +1,45 @@
 package frc.robot.subsystems;
 
-public class Pivot {
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Pivot extends SubsystemBase {
+  // TODO find actual values for these
+  private final double ENCODER_COUNTS_PER_REVOLUTION = 1.0;
+  private final double PIVOT_GEAR_RATIO = 1.0;
+
+  private final TalonSRX m_turret = new TalonSRX(5);
+  private final double minPivotAngle = -45.0;
+  private final double maxPivotAngle = 45.0;
+
+  public Pivot() {
+    m_turret.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.RemoteSensor0, 0, 10);
+  }
+
+  public void runMotor(double speed) {
+    double angle = m_turret.getSelectedSensorPosition() * ENCODER_COUNTS_PER_REVOLUTION * PIVOT_GEAR_RATIO;
+
+    if (angle < minPivotAngle && speed < 0) {
+      speed = 0.;
+    } else if (angle > maxPivotAngle && speed > 0) {
+      speed = 0;
+    }
+
+    m_turret.set(TalonSRXControlMode.PercentOutput, speed);
+  }
+
+  public void resetEncoder() {
+    m_turret.setSelectedSensorPosition(0.0);
+  }
+
+  public Command runMotorFactory(double speed) {
+    return runEnd(() -> runMotor(speed), () -> runMotor(0.0));
+  }
+
+  public Command resetEncoderFactory() {
+    return runOnce(this::resetEncoder);
+  }
 }
--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,4 +1,45 @@
 package frc.robot.subsystems;
 
-public class Turret {
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Turret extends SubsystemBase {
+  // TODO find actual values for these
+  private final double ENCODER_COUNTS_PER_REVOLUTION = 1.0;
+  private final double TURRET_GEAR_RATIO = 1.0;
+
+  private final TalonSRX m_turret = new TalonSRX(5);
+  private final double minTurretAngle = -45.0;
+  private final double maxTurretAngle = 45.0;
+
+  public Turret() {
+    m_turret.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.RemoteSensor0, 0, 10);
+  }
+
+  public void runMotor(double speed) {
+    double angle = m_turret.getSelectedSensorPosition() * ENCODER_COUNTS_PER_REVOLUTION * TURRET_GEAR_RATIO;
+
+    if (angle < minTurretAngle && speed < 0) {
+      speed = 0.;
+    } else if (angle > maxTurretAngle && speed > 0) {
+      speed = 0;
+    }
+
+    m_turret.set(TalonSRXControlMode.PercentOutput, speed);
+  }
+
+  public void resetEncoder() {
+    m_turret.setSelectedSensorPosition(0.0);
+  }
+
+  public Command runMotorFactory(double speed) {
+    return runEnd(() -> runMotor(speed), () -> runMotor(0.0));
+  }
+
+  public Command resetEncoderFactory() {
+    return runOnce(this::resetEncoder);
+  }
 }
